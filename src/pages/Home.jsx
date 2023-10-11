1c10efb3fcb3344f6ebbda1c8c585acf494ffeed
joshpled <joshuapleduc@gmail.com>
2023-10-11 17:29:32 -0400

    Add React Spring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import { Spring } from '@react-spring/web'
+import { Spring, animated } from '@react-spring/web'
 
 const Home = () => {
     return (
         <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            {style => <animated.div style={style} />}
+            {style => <animated.div style={style} key="animated-div" />}
         </Spring>
     )
 }
